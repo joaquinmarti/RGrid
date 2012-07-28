ceca3a2936625ea084d42e3dc457f2ce1d9595f7
Joaquín <joaquin@Joaquin-Martis-MacBook-Pro.local>
2012-07-28 16:46:43 +0200

    Responsive grids

--- js/rgrid.js
+++ js/rgrid.js
@@ -2,38 +2,30 @@ var RGrid = {};
 
 RGrid = (function () {
 
-  var options;
+  var options = {};
+  var resolutions = [];
+  var currentRes;
+
+  // Memory cache to
+  var cache = {
+    horizontal: [],
+    vertical: []
+  }
 
   var style = {
     horizontal: {
       position: 'absolute',
-      left: '0px',
+      left: '0',
       height: '1px',
       width: '100%',
       backgroundColor: '#00aaee'
     },
     vertical: {
       position: 'absolute',
-      top: '0px',
+      top: '0',
       width: '1px',
       backgroundColor: '#00aaee'
     }
-  };
-
-  /* Helpers */
-  var merge = function(ob1, ob2){ // Merge objects
-    for (var key in ob2) {
-      if (ob2.hasOwnProperty(key)) {
-        ob1[key] = ob2[key];
-      }
-    }
-    return ob1;
-  };
-
-  var getRes = function(className) { // Get resolution from className "grid_*"
-    var regexp = /([A-Za-z0-9]+)(_)([\d]+)/;
-    parts = regexp.exec(className);
-    return parts[3];
   };
 
   /* Build Grid */
@@ -42,16 +34,15 @@ RGrid = (function () {
       // Create grid container
       var gridContainer;
       gridContainer = document.createElement("div");
-      gridContainer.className = 'grid_' + res;
-      if (res != 1024)
+      gridContainer.id = 'grid_' + res;
       gridContainer.style.display = 'none';
 
       gridContainer.style = merge(gridContainer.style, {
         position: 'absolute',
         width: '100%',
         height: '1px',
-        top: '0px',
-        left: '0px',
+        top: '0',
+        left: '0',
         overflow: 'visible'
       });
 
@@ -64,15 +55,21 @@ RGrid = (function () {
       // Create horizontal lines
       hLines(gridContainer);
     }
+
+    // Set first resolution
+    evalCurrentRes();
+
+    // @todo: Add debug window
   };
 
   var vLines = function(gridContainer) {
     // Get resolution name
-    var res = getRes(gridContainer.className);
+    var res = getRes(gridContainer.id);
 
     // Get sizes
     var bodyWidth = document.body.scrollWidth;
     var gridWidth = options[res].width;
+    // @todo: Add calc to percentage columns
     var columnWidth = (gridWidth - (options[res].offset * 2) - ((options[res].columns - 1) * options[res].gutter)) / options[res].columns;
     var vlinesContainerStyle;
 
@@ -81,33 +78,33 @@ RGrid = (function () {
       case 'center':
         vlinesContainerStyle = {
           position: 'relative',
-          margin: '0px auto',
+          margin: '0 auto',
           width: gridWidth + 'px',
           height: '1px',
-          top: '0px',
-          left: '0px',
+          top: '0',
+          left: '0',
           overflow: 'visible'
         }
         break;
       case 'left':
         vlinesContainerStyle = {
           position: 'absolute',
-          margin: '0px',
+          margin: '0',
           width: gridWidth + 'px',
           height: '1px',
-          top: '0px',
-          left: '0px',
+          top: '0',
+          left: '0',
           overflow: 'visible'
         }
         break;
       case 'right':
         vlinesContainerStyle = {
           position: 'absolute',
-          margin: '0px',
+          margin: '0',
           width: gridWidth + 'px',
           height: '1px',
-          top: '0px',
-          right: '0px',
+          top: '0',
+          right: '0',
           overflow: 'visible'
         }
         break;
@@ -143,7 +140,7 @@ RGrid = (function () {
 
   var hLines = function(gridContainer) {
     // Get resolution name
-    var res = getRes(gridContainer.className);
+    var res = getRes(gridContainer.id);
 
     // Get sizes
     var baseline = parseInt(options[res].baseline);
@@ -175,6 +172,8 @@ RGrid = (function () {
 
     // Add vertical line to the grid
     container.appendChild(vline);
+
+    // @todo: Add line to cache
   };
 
   var drawHorizontal = function(top, container) {
@@ -187,6 +186,8 @@ RGrid = (function () {
 
     // Add horizontal line to the grid
     container.appendChild(hline);
+
+    // @todo: Add line to cache
   };
 
   /* Events */
@@ -195,24 +196,82 @@ RGrid = (function () {
   };
 
   var resize = function() {
-    // @todo: Add resize controls to make it responsive
+    window.onresize = function(event) {
+      evalCurrentRes();
+      // @todo: Change height: draw less or more horizontal lines
+    }
   };
 
-  /* Public API */
+  /* Change resolution grid */
+
+  var evalCurrentRes = function() {
+    var bodyWidth = document.body.scrollWidth;
+    var bodyHeight = document.body.scrollHeight;
+    var resolution;
+
+    for (i = resolutions.length - 1; i >= 0; i--) { // Loop inversely resolutions
+      if (bodyWidth >= resolutions[i]) {
+        resolution = resolutions[i]; // Set current break
+        break;
+      }
+    }
+
+    if (resolution != null && resolution != currentRes) {
+      currentRes = resolution;
+      setCurrentRes();
+    }
+  };
+
+  var setCurrentRes = function() {
+    // Hide all grid containers
+    for (var res in options) {
+      document.getElementById('grid_' + res).style.display = 'none';
+    }
+
+    // Show current resolution container
+    document.getElementById('grid_' + currentRes).style.display = 'block';
+  };
+
+  /* Helpers */
+
+  var merge = function(ob1, ob2){ // Merge objects
+    for (var key in ob2) {
+      if (ob2.hasOwnProperty(key)) {
+        ob1[key] = ob2[key];
+      }
+    }
+    return ob1;
+  };
+
+  var getRes = function(id) { // Get resolution from id "grid_*"
+    var regexp = /([A-Za-z0-9]+)(_)([\d]+)/;
+    parts = regexp.exec(id);
+    return parts[3];
+  };
+
+  /* Public function */
   return {
     init: function(userOptions) {
 
       // Merge default and user options
       options = merge({
         '1024': {
-          width: 1000,
           columns: 6,
+          columnUnit: 'px',
+          width: 1000,
           gutter: 20,
+          offset: 20,
           align: 'center',
-          baseline: 20,
-          offset: 20
+          baseline: 20 // Always in px
         }
       }, userOptions || {});
+
+      // Save and order resolutions
+      for (var res in options) {
+        resolutions.push(parseInt(res));
+      }
+
+      resolutions.reverse();
 
       // Draw all grids
       drawGrids();
